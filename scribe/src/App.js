f9eb8b969a3e310242f2cec9f7806321c9994276
Juliet Zhang <julietzhang3@gmail.com>
2022-05-22 03:54:15 -0700

    Add styling and key word section

--- scribe/src/App.js
+++ scribe/src/App.js
@@ -1,6 +1,7 @@
 import MicRecorder from "mic-recorder-to-mp3";
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+import "./App.css";
 
 // Set AssemblyAI Axios Header
 const assembly = axios.create({
@@ -18,16 +19,30 @@ function convert_ms(start_ms) {
   var minutes = Math.floor((start_ms / 60000) % 60).toFixed(0);
   var seconds = ((start_ms / 1000) % 60).toFixed(0);
   if (hours > 0) {
-    return (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  }
-  else {
-    return (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    return (
+      (hours < 10 ? "0" : "") +
+      hours +
+      ":" +
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds
+    );
+  } else {
+    return (
+      (minutes < 10 ? "0" : "") +
+      minutes +
+      ":" +
+      (seconds < 10 ? "0" : "") +
+      seconds
+    );
   }
 }
 
-function testPrint(t) {
-  console.log(t);
-}
+// function testPrint(t) {
+//   console.log(t);
+// }
 
 const App = () => {
   // Mic-Recorder-To-MP3
@@ -36,6 +51,7 @@ const App = () => {
   const [blobURL, setBlobUrl] = useState(null);
   const [audioFile, setAudioFile] = useState(null);
   const [isRecording, setIsRecording] = useState(null);
+  const [doneRecording, setDoneRecording] = useState(false);
 
   useEffect(() => {
     //Declares the recorder object and stores it inside of ref
@@ -46,10 +62,12 @@ const App = () => {
     // Check if recording isn't blocked by browser
     recorder.current.start().then(() => {
       setIsRecording(true);
+      <p>Recording in progress...</p>;
     });
   };
 
   const stopRecording = () => {
+    setDoneRecording(true);
     recorder.current
       .stop()
       .getMp3()
@@ -91,6 +109,7 @@ const App = () => {
       .post("/transcript", {
         audio_url: uploadURL,
         auto_chapters: true,
+        auto_highlights: true
       })
       .then((res) => {
         setTranscriptID(res.data.id);
@@ -130,28 +149,57 @@ const App = () => {
   return (
     <div>
       <h1>scribe.</h1>
-      <audio ref={audioPlayer} src={blobURL} controls="controls" />
-      <div>
-        <button disabled={isRecording} onClick={startRecording}>
-          Start Recording
+      <p class="italic margin-bottom faded">
+        Welcome to Scribe. <br />
+        To begin, simply click the start button below and we'll generate your
+        notes!
+      </p>
+      {doneRecording && (
+        <audio ref={audioPlayer} src={blobURL} controls="controls" />
+      )}
+      <div class="buttons">
+        <button
+          class="green-button"
+          disabled={isRecording}
+          onClick={startRecording}
+        >
+          <h3>start recording</h3>
         </button>
-        <button disabled={!isRecording} onClick={stopRecording}>
-          Stop Recording
+        <button
+          class="red-button"
+          disabled={!isRecording}
+          onClick={stopRecording}
+        >
+          <h3>stop recording</h3>
         </button>
-        <br/>
-        <button onClick={submitTranscriptionHandler}>Generate Results!</button>
+        <br />
+        {doneRecording && (
+          <button class="blue-button" onClick={submitTranscriptionHandler}>
+            <h3>transcribe!</h3>
+          </button>
+        )}
       </div>
       {transcriptData.status === "completed" ? (
         <div>
-          <h2>transcript</h2>
+          <h2 class="margin-top">transcript</h2>
           <p>{transcript}</p>
-          <h2>summary</h2>
+          <h2 class="margin-top">summary</h2>
           {transcriptData.chapters.map((chapter) => {
             return (
               <div>
-                {testPrint(chapter)}
-                <h3>{convert_ms(chapter.start)} - {convert_ms(chapter.end)}</h3>
+                {/* {testPrint(chapter)} */}
+                <h4 class="timestamp">
+                  {convert_ms(chapter.start)} - {convert_ms(chapter.end)}
+                </h4>
                 <p>{chapter.summary}</p>
+              </div>
+            );
+          })}
+          <h2 class="margin-top">key words</h2>
+          {transcriptData.auto_highlights_result.results.map((highlight) => {
+            return (
+              <div>
+                <p>{highlight.text}</p>
               </div>
             );
           })}
